fba239c81060b1e5c04967496f52fc7de52a5a20
Bianca Rocha <Bianca Rocha>
2017-04-12 22:33:56 -0300

    ajustes e refatoramento de funções

--- index.js
+++ index.js
@@ -12,10 +12,7 @@
 //Valida os campos nome, empresa e time
 (function()
 {
-  var button = document.getElementsByClassName('add');
-  button = button[0];
-
-  button.addEventListener('click', function() 
+  function valida_campos() 
   {
     var nome = document.getElementsByName('nome');
     var empresa = document.getElementsByName('empresa');
@@ -23,34 +20,54 @@
     var idade = document.getElementsByName('idade');
     var tecnologias = document.getElementsByName('tec');
     var estagio = document.getElementsByName('smoker');
+    var valido = true;
     
     //Verifica se os campos nome, empresa e time estão preenchidos
     if(nome[0].value == '' && empresa[0].value == '' || empresa[0].value == '' && time[0].value == ''  || time[0].value == '' && nome[0].value == '') {
       console.log('Por favor, preencha os campos obrigatórios');
+      valido = false;
     }
 
     else if(nome[0].value == '') {
        console.log('Por favor, preencha o seu nome');
+       valido = false;
     }
 
     else if(empresa[0].value == '') {
       console.log('Por favor, preencha o nome da empresa');
+      valido = false;
     }
 
     else if(time[0].value == '') {
       console.log('Por favor, preencha o seu time de desenvolvimento');
+      valido = false;
     }
 
     //Verifica se a idade é numérica e maior que 0
     idade = parseInt(idade[0].value);
     if(idade === 0 || idade < 0  || isNaN(idade) == true) {
       console.log('idade inválida');
+      valido = false;
     }
 
-    //Transforma em JSON
-    var json = JSON.stringify({empresa: empresa[0].value, nome: nome[0].value, idade: idade, time: time[0].value, tecnologias: tecnologias[0].value, estagio: estagio[0].checked }, null, '\t');
-    console.log(json);
-  });
+    //Se as condições anteriores forem atendidas, mostra o json
+    if(valido === true)
+    {
+      //Transforma em JSON
+      var json = JSON.stringify({empresa: empresa[0].value, nome: nome[0].value, idade: idade, time: time[0].value, tecnologias: tecnologias[0].value, estagio: estagio[0].checked }, null, '\t');
+      console.log(json);
+      
+      //Mostra o JSON serializado no elemento debug
+      var debug = document.getElementsByClassName('debug');
+      debug[0].innerHTML = json;
+    }
+  };
+
+  //Dispara o evento ao clique
+  var button = document.getElementsByTagName('button');
+  for(var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', valida_campos);
+  }
 })();
 
 
--- listaDesenvolvedores.js
+++ listaDesenvolvedores.js
@@ -6,13 +6,10 @@
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
      var data = JSON.parse(request.responseText);
-     var nome = document.getElementsByClassName('nome');
-     var empresa = document.getElementsByClassName('empresa');
      var tabela = document.getElementsByClassName('table-hover');
-     console.log(tabela[0]);
       for(var i = 0; i < data.length; i++)
       {
-        tabela[0].innerHTML = tabela[0].innerHTML + '<tr> <td class="text-left nome">' + data[i].nome + '</td>' + '<td class="text-left empresa">' + data[i].empresa + '</td></tr>';          
+        tabela[0].innerHTML = tabela[0].innerHTML + '<tr> <td class="text-left">' + data[i].nome + '</td>' + '<td class="text-left">' + data[i].empresa + '</td></tr>';          
       } 
     } else {
       console.log("error")
